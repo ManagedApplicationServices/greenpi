d601da6a4741f711959ddb68eb0908ca4f438a88
Sayanee <sayanee@gmail.com>
2014-03-24 13:04:40 +0800

    connected redis

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var redis = require('redis');
+var client = redis.createClient();
+
 var IndexModel = require('../models/index');
 var TreeModel = require('../models/tree');
 var MessageModel = require('../models/message');
@@ -9,14 +12,22 @@ module.exports = function (app) {
   var model = new IndexModel();
   var tree = new TreeModel();
   var message = new MessageModel();
-  console.log(message);
+
+  var subtitle = ''
+
+  client.set('subtitle', 'raising environmental consciousness within an organization', redis.print);
+
+  client.get('subtitle', function (err, reply) {
+    subtitle = reply.toString();
+  });
 
   app.get('/', function (req, res) {
     res.render('index',
       {
         model: model,
         trees: tree,
-        messages: message
+        messages: message,
+        subtitle: subtitle
       }
     );
   });
