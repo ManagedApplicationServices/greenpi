13608a89d4360528634cf06be1bfafc27583f3ef
Sayanee <sayanee@gmail.com>
2014-03-25 17:37:32 +0800

    redid stores the current tree size after emitting the value through socket io

--- controllers/index.js
+++ controllers/index.js
@@ -4,17 +4,14 @@ var redis = require('redis');
 var client = redis.createClient();
 
 var IndexModel = require('../models/index');
-var TreeModel = require('../models/tree');
 var MessageModel = require('../models/message');
 
 module.exports = function (app) {
 
   var model = new IndexModel();
-  var tree = new TreeModel();
   var message = new MessageModel();
 
   var subtitle = '';
-  var tree1 = 0;
 
   client.set('subtitle', 'raising environmental consciousness within an organization', redis.print);
 
@@ -22,20 +19,12 @@ module.exports = function (app) {
     subtitle = reply.toString();
   });
 
-  client.set('tree1', 100);
-
-  client.decr('tree1', function (err, reply) {
-    tree1 = reply;
-  });
-
   app.get('/', function (req, res) {
     res.render('index',
       {
         model: model,
-        trees: tree,
         messages: message,
-        subtitle: subtitle,
-        tree1: tree1
+        subtitle: subtitle
       }
     );
   });
