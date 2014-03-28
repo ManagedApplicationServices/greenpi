c0e2532fa2c63c14d89190d25d2a5bf46d25ee99
Sayanee <sayanee@gmail.com>
2014-03-28 15:31:48 +0800

    dynamic messages

--- models/message.js
+++ models/message.js
@@ -4,9 +4,10 @@ module.exports = function MessageModel() {
   return {
     messages:
     [
-      { id: 1, content: 'Earth our home' },
+      { id: 1, content: 'Welcome to RICOH\'s forest! Less printing will keep this forest.' },
       { id: 2, content: 'Shred paper, recycle!' },
-      { id: 3, content: 'Use renewable forest' }
+      { id: 3, content: 'Use renewable forest' },
+      { id: 4, content: 'Do you know 17 trees are needed for 1 tonne of paper?' }
     ]
   };
 };
