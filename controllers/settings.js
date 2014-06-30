790d7fb15112872131d0649c6e2969f8aa0c3690
Sayanee <sayanee@gmail.com>
2014-06-30 12:35:54 +0800

    admin can set configurations for printer and upload poster images

--- controllers/settings.js
+++ controllers/settings.js
@@ -2,91 +2,92 @@
 
 var SettingModel = require('../models/setting');
 var config = {};
-var file = '../config.json';
+var configFile = './config.json';
+var fs = require('fs');
 
-module.exports = function (router) {
-    var model = new SettingModel();
+module.exports = function (app) {
+  var model = new SettingModel();
 
-    router.get('/admin', function (req, res) {
-      res.render('setting', model);
-    });
+  app.get('/admin', function (req, res) {
+    res.render('setting', model);
+  });
 
-    router.post('/admin', function(req, res) {
-      var companyLogoFile = req.files.companyLogo;
-      var poster1File = req.files.poster1;
-      var poster2File = req.files.poster2;
-      var poster3File = req.files.poster3;
-      var poster4File = req.files.poster4;
-      var poster5File = req.files.poster5;
-      var newPath = '';
+  app.post('/admin', function(req, res) {
+    var companyLogoFile = req.files.companyLogo;
+    var uploadedImages = new Array();
+    var i = 0;
 
-      console.log(req.body.printerIp);
-      console.log(req.body.organisationCap);
-      console.log(req.body.totalPrinters);
+    console.log(req.files);
+    console.log(req.body.printerIP);
+    console.log(req.body.organisationCap);
+    console.log(req.body.totalPrinters);
 
-      fs.readFile(file, 'utf8', function (err, data) {
-        if (err) {console.log('Error: ' + err); return; }
+    fs.readFile(configFile, 'utf8', function (err, data) {
+      if (err) {console.log('Error: ' + err); return; }
+      config = JSON.parse(data);
+      console.log('CONFIG:');
+      console.log(config);
 
-        config = JSON.parse(data);
+      // create new config file based on admin input
+      var newConfig = createNewConfig(config, req.body);
 
-
+      fs.writeFile(configFile, JSON.stringify(newConfig, null, 4), function(err) {
+        if(err) {
+          console.log(err);
+        } else {
+          console.log('JSON saved to ' + configFile);
+        }
       });
 
-      if(companyLogoFile.size > 0) {
-        newPath = appPath + '/public/img/logo.jpg';
-        require('fs').rename(companyLogoFile.path, newPath,
-          function(error) {
-            if(error) {res.send({ error: 'Ah crap! Something bad happened'}); return; }
-          }
-        );
-      }
+      // transfer posters and logos based on admin upload
+      transferUploadedImages(req.files, config);
 
-      if(poster1File.size > 0) {
-        newPath = appPath + '/public/img/poster-1.jpg';
-        require('fs').rename(poster1File.path, newPath,
-          function(error) {
-            if(error) {res.send({ error: 'Ah crap! Something bad happened'}); return; }
-          }
-        );
-      }
-
-      if(poster2File.size > 0) {
-        newPath = appPath + '/public/img/poster-2.jpg';
-        require('fs').rename(poster2File.path, newPath,
-          function(error) {
-            if(error) {res.send({ error: 'Ah crap! Something bad happened'}); return; }
-          }
-        );
-      }
-
-      if(poster3File.size > 0) {
-        newPath = appPath + '/public/img/poster-3.jpg';
-        require('fs').rename(poster3File.path, newPath,
-          function(error) {
-            if(error) {res.send({ error: 'Ah crap! Something bad happened'}); return; }
-          }
-        );
-      }
-
-      if(poster4File.size > 0) {
-        newPath = appPath + '/public/img/poster-4.jpg';
-        require('fs').rename(poster4File.path, newPath,
-          function(error) {
-            if(error) {res.send({ error: 'Ah crap! Something bad happened'}); return; }
-          }
-        );
-      }
-
-      if(poster5File.size > 0) {
-        newPath = appPath + '/public/img/poster-5.jpg';
-        require('fs').rename(poster5File.path, newPath,
-          function(error) {
-            if(error) {res.send({ error: 'Ah crap! Something bad happened'}); return; }
-          }
-        );
-      }
-
-      res.render('setting-done', model);
     });
 
+    res.render('setting-done', model);
+  });
+
 };
+
+function transferUploadedImages(uploadedImages, config) {
+  var newPath = '';
+
+  for(var i in uploadedImages){
+    // console.log(i); // key
+    // console.log(uploadedImages[i]); // value
+
+    if(uploadedImages[i].size > 0) {
+      newPath = config.appPath + '/public/img/' + i + '.jpg';
+      // console.log(newPath);
+      require('fs').rename(uploadedImages[i].path, newPath,
+        function(error) {
+          var errorMessage = 'Ah crap! Could not load ' + i + ' :(';
+          if(error) {
+            res.send( {error: errorMessage } );
+            return;
+          }
+        }
+      );
+    }
+
+  }
+
+}
+
+function createNewConfig(config, req) {
+  var newConfig = config;
+
+  if(req.printerIP) {
+    newConfig.printerIP = req.printerIP;
+  }
+
+  if(req.organisationCap) {
+    newConfig.paperUsageCap = req.organisationCap;
+  }
+
+  if(req.totalPrinters) {
+    newConfig.totalPrinters = req.totalPrinters;
+  }
+
+  return newConfig;
+}
