5638eb2cadcd775ad25f16ae97d114c1ba3f97f0
Sayanee <sayanee@gmail.com>
2014-09-26 12:07:44 +0800

    simulation reset tested

--- test/lib/simulationSpec.js
+++ test/lib/simulationSpec.js
@@ -3,51 +3,95 @@
 var expect = require('expect.js'),
   simulation = require('../../lib/simulation');
 
-describe('Simulation', function() {
+describe('Simulation: ', function() {
 
-  it('returns a demo printer model if in demo mode', function(done) {
-    var data = {
-      demo: true
-    };
+  describe('Get printer info: ', function() {
 
-    simulation.getPrinterInfo(data, function(forest) {
-      expect(forest.printerModel).to.equal('printer');
-      done();
-    });
-  });
-
-  it('returns a demo printer ID if in demo mode', function(done) {
+    it('returns a demo printer model if in demo mode', function(done) {
       var data = {
         demo: true
       };
 
       simulation.getPrinterInfo(data, function(forest) {
-        expect(forest.printerID).to.equal('demo');
+        expect(forest.printerModel).to.equal('printer');
         done();
       });
     });
 
-  it('returns the printer model if not in demo mode', function(done) {
-    var data = {
-      demo: false,
-      printerInfoUrl: 'http://localhost:8080/printer-info.html'
-    };
+    it('returns a demo printer ID if in demo mode', function(done) {
+        var data = {
+          demo: true
+        };
 
-    simulation.getPrinterInfo(data, function(forest) {
-      expect(forest.printerModel).to.equal('MP C6502');
-      done();
+        simulation.getPrinterInfo(data, function(forest) {
+          expect(forest.printerID).to.equal('demo');
+          done();
+        });
+      });
+
+    it('returns the printer model if not in demo mode', function(done) {
+      var data = {
+        demo: false,
+        printerInfoUrl: 'http://localhost:8080/printer-info.html'
+      };
+
+      simulation.getPrinterInfo(data, function(forest) {
+        expect(forest.printerModel).to.equal('MP C6502');
+        done();
+      });
+    });
+
+    it('returns the printer ID if not in demo mode', function(done) {
+      var data = {
+        demo: false,
+        printerInfoUrl: 'http://localhost:8080/printer-info.html'
+      };
+
+      simulation.getPrinterInfo(data, function(forest) {
+        expect(forest.printerID).to.equal('E233C850036');
+        done();
+      });
     });
 
   });
 
-  it('returns the printer ID if not in demo mode', function(done) {
-    var data = {
-      demo: false,
-      printerInfoUrl: 'http://localhost:8080/printer-info.html'
-    };
+  describe('Reset: ', function() {
 
-    simulation.getPrinterInfo(data, function(forest) {
-      expect(forest.printerID).to.equal('E233C850036');
+    var forest = {};
+
+    beforeEach(function(done) {
+      forest = {};
+      done();
+    })
+
+    it('returns a forest object', function(done) {
+      expect(simulation.reset(forest)).to.be.an('object');
+      done();
+    });
+
+    it('returns zero for simulation livePrinterCount', function(done) {
+      expect(simulation.reset(forest).livePrinterCount).to.be.zero;
+      done();
+    });
+
+    it('returns zero for simulation count', function(done) {
+      expect(simulation.reset(forest).count).to.be.zero;
+      done();
+    });
+
+    it('returns zero for simulation last', function(done) {
+      expect(simulation.reset(forest).last).to.be.zero;
+      done();
+    });
+
+    it('returns current time for simulation start time', function(done) {
+      var now = new Date();
+      expect(simulation.reset(forest).simulationStartDatetime.toISOString()).to.be.equal(now.toISOString());
+      done();
+    });
+
+    it('returns zero for simulation offset', function(done) {
+      expect(simulation.reset(forest).offset).to.be.zero;
       done();
     });
 
