df55314b77afe0146213c6bb7033de9beffa71d6
Sayanee <sayanee@gmail.com>
2015-03-18 17:18:03 +0800

    test: start dbSpec test

---
+++ test/lib/dbSpec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var expect = require('chai').expect;
+var adminLib = require('../../lib/db');
+
+describe('From DB Library', function() {
+  describe('#reset', function() {
+    it('resets db')
+  })
+})
