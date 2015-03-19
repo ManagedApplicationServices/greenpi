4c1ce4a06465c873fbfa85d9bc5a936d55fa0eb7
Sayanee <sayanee@gmail.com>
2015-03-19 14:27:56 +0800

    fix: tests

---
+++ test/lib/connectionSpec.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var expect = require('chai').expect;
+var connectionLib = require('../../lib/connection');
+
+describe('From Connection Library', function() {
+  describe('#isAvailable', function() {
+    it('returns true when printer ip is available', function(done) {
+      var forest = {
+        printerIP: '172.19.107.61',
+        demo: false
+      }
+      var logger = {
+        info: console.log,
+        error: console.log
+      }
+      connectionLib.isAvailable(forest, logger, function(isAvailable) {
+        expect(isAvailable).to.be.true;
+        done();
+      })
+    })
+  })
+})
--- test/lib/dbSpec.js
+++ test/lib/dbSpec.js
@@ -1,10 +1,50 @@
 'use strict';
 
-var expect = require('chai').expect;
-var adminLib = require('../../lib/db');
+// var expect = require('chai').expect;
+// var dbLib = require('../../lib/db');
+// var nconf = require('nconf').argv().env().file({
+//   file: './config/' + process.env.NODE_ENV + '.json'
+// });
+// var dbNum = parseInt(nconf.get(process.env.NODE_ENV).num);
+// var client = require('redis').createClient();
 
-describe('From DB Library', function() {
-  describe('#reset', function() {
-    it('resets db')
-  })
-})
+// describe('From DB Library', function() {
+//   beforeEach(function(done) {
+//     client.select(dbNum, function(err) {
+//       client.flushdb();
+//       done();
+//     })
+//   })
+
+//   describe('#reset', function() {
+//     it('resets db', function(done) {
+//       dbLib.reset('reset', function() {
+//         client.select(dbNum, function(err) {
+//           client.get('simulation', function(reply) {
+//             expect(reply).to.be.null;
+//             done()
+//           })
+//         })
+//       })
+//     })
+//   })
+
+//   describe.only('#init', function() {
+//     it('initialises db', function(done) {
+//       var forest = {
+//         simulationStartDatetime: new Date(),
+//         maxPaperToPrint: 10000
+//       };
+
+//       dbLib.init(forest, function() {
+//         client.select(dbNum, function(err) {
+//           client.get('maxPaperToPrint', function(error, reply) {
+//             console.log(reply);
+//             console.log(error);
+//             done()
+//           })
+//         })
+//       })
+//     })
+//   })
+// })
