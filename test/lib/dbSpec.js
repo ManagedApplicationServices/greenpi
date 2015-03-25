4a4ea22733127a270edc639e93098ab472f23774
Sayanee <sayanee@gmail.com>
2015-03-25 10:26:23 +0800

    test: more test

--- test/lib/dbSpec.js
+++ test/lib/dbSpec.js
@@ -1,50 +1,50 @@
 'use strict';
 
-// var expect = require('chai').expect;
-// var dbLib = require('../../lib/db');
-// var nconf = require('nconf').argv().env().file({
-//   file: './config/' + process.env.NODE_ENV + '.json'
-// });
-// var dbNum = parseInt(nconf.get(process.env.NODE_ENV).num);
-// var client = require('redis').createClient();
+var expect = require('chai').expect;
+var dbLib = require('../../lib/db');
+var nconf = require('nconf').argv().env().file({
+  file: './config/' + process.env.NODE_ENV + '.json'
+});
+var dbNum = parseInt(nconf.get(process.env.NODE_ENV).num);
+var client = require('redis').createClient();
 
-// describe('From DB Library', function() {
-//   beforeEach(function(done) {
-//     client.select(dbNum, function(err) {
-//       client.flushdb();
-//       done();
-//     })
-//   })
+describe('From DB Library', function() {
+  beforeEach(function(done) {
+    client.select(dbNum, function() {
+      client.flushdb();
+      done();
+    })
+  })
 
-//   describe('#reset', function() {
-//     it('resets db', function(done) {
-//       dbLib.reset('reset', function() {
-//         client.select(dbNum, function(err) {
-//           client.get('simulation', function(reply) {
-//             expect(reply).to.be.null;
-//             done()
-//           })
-//         })
-//       })
-//     })
-//   })
+  describe('#reset', function() {
+    it('resets db', function(done) {
+      dbLib.reset('reset', function() {
+        client.select(dbNum, function() {
+          client.get('simulation', function(reply) {
+            expect(reply).to.be.null;
+            done()
+          })
+        })
+      })
+    })
+  })
 
-//   describe.only('#init', function() {
-//     it('initialises db', function(done) {
-//       var forest = {
-//         simulationStartDatetime: new Date(),
-//         maxPaperToPrint: 10000
-//       };
+  describe('#init', function() {
+    it('initialises db', function(done) {
+      var forest = {
+        simulationStartDatetime: new Date(),
+        maxPaperToPrint: 10000
+      };
 
-//       dbLib.init(forest, function() {
-//         client.select(dbNum, function(err) {
-//           client.get('maxPaperToPrint', function(error, reply) {
-//             console.log(reply);
-//             console.log(error);
-//             done()
-//           })
-//         })
-//       })
-//     })
-//   })
-// })
+      dbLib.init(client, forest, function() {
+        client.select(dbNum, function() {
+          client.get('maxPaperToPrint', function(error, reply) {
+            expect(reply).to.be.null;
+            expect(error).to.be.null;
+            done();
+          })
+        })
+      })
+    })
+  })
+})
